b5d2b14418a5b560861d6a3c2cc1923da9a6e71e
Helena <helena.gomez@thoughtworks.com>
2022-11-22 11:02:49 +0100

    Make it clean code

--- iteration/repeat.go
+++ iteration/repeat.go
@@ -1,9 +1,11 @@
 package iteration
 
+const repeatCount = 5
+
 func Repeat(chart string) string {
 	var repeated string
-	for i := 0; i < 5; i++ {
-		repeated = repeated + chart
+	for i := 0; i < repeatCount; i++ {
+		repeated += chart
 
 	}
 	return repeated
